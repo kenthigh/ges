ab2d51a93ea9e53e29183229d620f941c146cfa5
gaoyasong <gaoyasong@vipkid.com.cn>
2017-12-11 18:55:53 +0800

    按钮优化1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){$("#btn1").click(function(){console.log("sd")})});
+$(document).ready(function(){$("#btn1").click(function(){console.log("sd")}),$("#btn1").hover(function(){},function(){})});
 //# sourceMappingURL=map/main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $('#btn1').click(function() {
       console.log('sd')
     //   $('#audio1')[0].play()
-  })   
+  })
+  $('#btn1').hover(function(){
+  }, function(){
+  })
 })
